031332d373b99e3cc0b259db6f3854dd1485a6ec
Angel Mendez <id.angelmendez@gmail.com>
2024-05-16 13:44:41 -0700

    Added game manager class

---
+++ src/GameManager.java
@@ -0,0 +1,12 @@
+public class GameManager {
+    // This class is going to be in charge of a playthrough from start to finish
+    // will have methods such as initialize game, etc etc
+    public GameManager() {
+
+    }
+
+    public void initializeGame() {
+        // in here we will handle all game initialization
+        // we can meet to discuss what that might mean for our game
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-
-
+        GameManager game = new GameManager();
+        // GameManager class will have actual logic for game
+        // the main method in Main class will pretty much only be doing
+        // method class / class instantiations
     }
 }
